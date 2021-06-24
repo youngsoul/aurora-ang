39dabb939faf2d699f82c179d07a1ac722a7fcb3
pat.ryan <pat.ryan@spr.com>
2021-06-24 17:38:54 -0500

    added simple hook to do a get request

---
+++ dev_readme.txt
@@ -0,0 +1,13 @@
+ng new aurora-ang
+cd aurora-ang
+ng serve --open
+
+ng build
+
+-- go to dist folder created
+
+-- copy dist contents to s3
+
+aws s3 sync dist/aurora-ang/ s3://pryan-spa-test-1 --profile spr
+
+aws --region us-east-1 --profile spr cloudfront create-invalidation --distribution-id  E2SS2XIS0ASHOU --paths '/*'
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aurora-ang';
+  customers = [];
+  cust_string = "";
+  constructor(private http:HttpClient) {
+
+  }
+
+  got_data() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin':'*',
+      })
+    };
+    // -----------------  ADD YOUR URL TO THE GET REQUEST  --------
+    // UPDATEME
+    this.http.get("").subscribe((data:any) => this.customers = data);
+    this.cust_string = JSON.stringify(this.customers)
+    console.log(this.cust_string);
+  }
 }
